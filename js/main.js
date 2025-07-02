6d6dc4ccadff3b20deecc65e9924053655234dcd
tarektawfiq <tareektawfiq40@gmail.com>
2025-07-02 14:19:19 +0300

    XXX

--- js/main.js
+++ js/main.js
@@ -93,4 +93,10 @@ function exportLocalStorageToJson(filename = "data.json") {
     console.error("Failed to parse 'Product' data:", error);
   }
 }
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("service-worker.js")
+    .then((reg) => console.log("Service Worker registered:", reg.scope))
+    .catch((err) => console.error("Service Worker error:", err));
+}
 exportLocalStorageToJson("myLocalStorageData.json");
